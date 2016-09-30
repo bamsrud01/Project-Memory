4700043c6d43e21e485cb8bc1c9bf1d55526a3a9
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-30 18:43:56 -0500

    Built basic HTML, commented outline to JS file

---
+++ app.js
@@ -0,0 +1,21 @@
+/*    MEMORY    */
+//  Barrett Amsrud
+//  October, 2016
+
+
+//Create one array of sets of two numbers.
+var array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15];
+var clickCount = 0;
+var totalTurns = 0;
+
+$(document).ready(function() {
+
+//click handler
+
+});
+
+//function to shuffle array
+
+//function to append divs to the DOM
+
+//function to hide revealed cards

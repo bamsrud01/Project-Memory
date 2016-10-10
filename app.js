b7809ca46d787e0c13979f8e6e6e7747f5e177c9
Barrett Amsrud <barrettamsrud@gmail.com>
2016-10-10 14:05:28 -0500

    Altered length of array

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 
 //Create one array of sets of two numbers.
-var cardArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15];
+var cardArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12];
 var clickCount = 0;
 var totalTurns = 0;
 

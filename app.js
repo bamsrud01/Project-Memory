3779125731a087b6edb62126a7611a2ae504f30e
Barrett Amsrud <barrettamsrud@gmail.com>
2016-10-27 21:27:13 -0500

    Outlined click function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*    MEMORY    */
+
 //  Barrett Amsrud
 //  October, 2016
-
 
 //  Create one array of sets of two numbers.
 var cardArray = ['&#x16A0', '&#x16A2', '&#x16A6', '&#x16A8', '&#x16B1', '&#x16B2',
@@ -11,15 +11,29 @@ var cardArray = ['&#x16A0', '&#x16A2', '&#x16A6', '&#x16A8', '&#x16B1', '&#x16B2
 var clickCount = 0;
 var totalTurns = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
+
   //  Duplicate all items in the array
   duplicateArray(cardArray);
+
   //  Shuffle cards
   shuffleArray(cardArray);
+
   //  Append cards to DOM
   placeCards(cardArray);
 
+  //  Hide all cards
+  hideCards($('.card'));
+
   //  Click handler
+  $('#board').on('click', '.card', function() {
+    //  If hidden
+
+    //  If counter shows 2 cards
+      //  Check for a match
+      //  If match, add permanent class
+      //  If not, reset the counter
+  });
 
 });
 
@@ -33,13 +47,14 @@ function duplicateArray(array) {
 
 //  Function to shuffle array.  Found on Stack Overflow.
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
 }
 
 //  Function to append divs to the DOM
@@ -48,12 +63,14 @@ function placeCards(array) {
   //  Create six rows
   for (var rows = 0; rows < 6; rows++) {
     var $row = $('<div class="card-row"></div>');
+
     //  Add cards to rows
     for (var cardItem = 0; cardItem < cardsPerRow; cardItem++) {
       //  Add cards
       var cardInfo = array.shift();
       $row.append('<div class="card"><p>' + cardInfo + '</p></div>');
     }
+
     //  Append cards and reset variable
     $('#board').append($row);
     $row = null;
@@ -62,11 +79,6 @@ function placeCards(array) {
 }
 
 //  Function to hide revealed cards
-
-
-
-
-
-
-
-//
+function hideCards($card) {
+  $card.addClass('hidden');
+}
